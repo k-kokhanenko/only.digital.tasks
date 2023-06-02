620fea91cf5be7e5b029a1b281e6df18efeeacc9
Konstantin <k.kokhanenko@yandex.ru>
2023-06-02 10:36:54 +0300

    Add files via upload
    Update agent code

--- php_interface/init.php
+++ php_interface/init.php
@@ -6,11 +6,38 @@ AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("IBlockElementHandler
 AddEventHandler("iblock", "OnAfterIBlockElementUpdate", Array("IBlockElementHandler", "OnAfterIBlockElementUpdateHandler"));
 
 
-
 function myAgent() {
-    // интервал сейчас стоит в админ панели 300 - это 5 минут
-    // по заданию раз в час надо установить 3600 секунд!!!!
-    file_put_contents( $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/myagent.log', 'запуск агента - '.date('Y-m-d H:i:s').PHP_EOL, FILE_APPEND);
+    $iblockCode = 'LOG';
+
+    if (\Bitrix\Main\Loader::includeModule('iblock')) {
+        // Определяем id инфоблока 'LOG'
+        $arIblock = \Bitrix\Iblock\IblockTable::getList(
+            array('filter' => array('CODE' => $iblockCode))
+        )->fetch();
+
+        if (!empty($arIblock)) {
+            $iblockElements = CIBlockElement::GetList(
+                ['ACTIVE_FROM' => 'DESC'], 
+                ['IBLOCK_ID'=>$arIblock['ID']], 
+                false,
+                false, 
+                ['ID']
+            );
+
+            $count = 0;
+            while ($iblockElement = $iblockElements->GetNext())
+            {
+                $count++;
+
+                if ($count <= 10) {
+                    continue;
+                }
+
+                // Удаляем элемент инфоблока
+                CIBlockElement::Delete($iblockElement['ID']);
+            }
+        }
+    }
 
     return 'myAgent();';
 }
